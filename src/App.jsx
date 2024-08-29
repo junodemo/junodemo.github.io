da56dba541360bc9450b30d3e3402e4c34c2f923
Mike Saburenkov <mikesub@users.noreply.github.com>
2024-08-29 19:00:43 +0200

    init vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-xl font-bold">
+        JUNO demo apps
+      </h1>
+      <ul>
+        <li><a href="#">first</a></li>
+      </ul>
+    </>
+  )
+}
+
+export default App
