66960aba235459c2131fdf2393dc77bbf40d8178
Mike Saburenkov <mikesub@users.noreply.github.com>
2024-08-29 21:05:29 +0200

    add router and sample fiddles

---
+++ src/fiddles/index.js
@@ -0,0 +1,7 @@
+import First from './first';
+import Second from './second';
+
+export default [
+	{path: '/first', element: First, title: 'First title'},
+	{path: '/second', element: Second, title: 'Second title'}
+]
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,37 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import '../node_modules/@juno/uikit/dist/uikit.css';
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+
+import fiddles from './fiddles/index'
+
+const fiddlesRoutes = fiddles.map(f => ({path: f.path, element: React.createElement(f.element)}));
+
+function Root() {
+  return (
+    <ul>
+      {
+        fiddles.map((f, i) => 
+          <li key={i}><Link to={f.path}>{f.title}</Link></li>
+        )
+      }
+    </ul>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+  },
+  ...fiddlesRoutes
+]);
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
+
